35220906f7ca20900af4c8f7ca7ffe8f56c04d1d
Felix Thielen <felix.thielen@gmail.com>
2022-06-05 21:18:13 +0200

    implement base64_attachments: Message.parse() takes `signal: SignalAPI` instance as parameter, which allows us to call its new get_attachment() method. As a result, the Message.parse() needs to be `async` now.

--- signalbot/message.py
+++ signalbot/message.py
@@ -2,6 +2,9 @@ import json
 from enum import Enum
 
 
+from signalbot.api import SignalAPI
+
+
 class MessageType(Enum):
     SYNC_MESSAGE = 1
     DATA_MESSAGE = 2
@@ -56,7 +59,7 @@ class Message:
         return bool(self.group)
 
     @classmethod
-    def parse(cls, raw_message: str):
+    async def parse(cls, signal: SignalAPI, raw_message: str):
         try:
             raw_message = json.loads(raw_message)
         except Exception:
@@ -82,6 +85,7 @@ class Message:
             mentions = cls._parse_mentions(
                 raw_message["envelope"]["syncMessage"]["sentMessage"]
             )
+            base64_attachments = None
 
         # Option 2: dataMessage
         elif "dataMessage" in raw_message["envelope"]:
@@ -90,12 +94,12 @@ class Message:
             group = cls._parse_group_information(raw_message["envelope"]["dataMessage"])
             reaction = cls._parse_reaction(raw_message["envelope"]["dataMessage"])
             mentions = cls._parse_mentions(raw_message["envelope"]["dataMessage"])
+            base64_attachments = await cls._parse_attachments(
+                signal, raw_message["envelope"]["dataMessage"]
+            )
 
         else:
             raise UnknownMessageFormatError
-
-        # TODO: base64_attachments
-        base64_attachments = []
 
         return cls(
             source,
@@ -108,6 +112,17 @@ class Message:
             mentions,
             raw_message,
         )
+
+    @classmethod
+    async def _parse_attachments(cls, signal: SignalAPI, data_message: dict) -> str:
+
+        if "attachments" not in data_message:
+            return []
+
+        return [
+            await signal.get_attachment(attachment["id"])
+            for attachment in data_message["attachments"]
+        ]
 
     @classmethod
     def _parse_sync_message(cls, sync_message: dict) -> str:

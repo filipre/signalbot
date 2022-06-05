b73c9abe811313eb959beaec24716e252992d055
Felix Thielen <felix.thielen@gmail.com>
2022-06-05 21:24:31 +0200

    add a test case for messages with attachments

--- tests/test_message.py
+++ tests/test_message.py
@@ -1,5 +1,9 @@
+import base64
 import unittest
+from unittest.mock import AsyncMock, patch
+
 from signalbot import Message, MessageType
+from signalbot.api import SignalAPI
 
 
 class TestMessage(unittest.TestCase):
@@ -7,11 +11,24 @@ class TestMessage(unittest.TestCase):
     raw_data_message = '{"envelope":{"source":"+490123456789","sourceNumber":"+490123456789","sourceUuid":"<uuid>","sourceName":"<name>","sourceDevice":1,"timestamp":1632576001632,"dataMessage":{"timestamp":1632576001632,"message":"Uhrzeit","expiresInSeconds":0,"viewOnce":false,"mentions":[],"attachments":[],"contacts":[],"groupInfo":{"groupId":"<groupid>","type":"DELIVER"}}}}'  # noqa
     raw_reaction_message = '{"envelope":{"source":"<source>","sourceNumber":"<source>","sourceUuid":"<uuid>","sourceName":"<name>","sourceDevice":1,"timestamp":1632576001632,"syncMessage":{"sentMessage":{"timestamp":1632576001632,"message":null,"expiresInSeconds":0,"viewOnce":false,"reaction":{"emoji":"👍","targetAuthor":"<target>","targetAuthorNumber":"<target>","targetAuthorUuid":"<uuid>","targetSentTimestamp":1632576001632,"isRemove":false},"mentions":[],"attachments":[],"contacts":[],"groupInfo":{"groupId":"<groupid>","type":"DELIVER"},"destination":null,"destinationNumber":null,"destinationUuid":null}}}}'  # noqa
     raw_user_chat_message = '{"envelope":{"source":"+490123456789","sourceNumber":"+490123456789","sourceUuid":"<uuid>","sourceName":"<name>","sourceDevice":1,"timestamp":1632576001632,"dataMessage":{"timestamp":1632576001632,"message":"Uhrzeit","expiresInSeconds":0,"viewOnce":false}},"account":"+49987654321","subscription":0}'  # noqa
+    raw_attachment_message = '{"envelope":{"source":"+490123456789","sourceNumber":"+490123456789","sourceUuid":"<uuid>","sourceName":"<name>","sourceDevice":1,"timestamp":1632576001632,"dataMessage":{"timestamp":1632576001632,"message":"Uhrzeit","expiresInSeconds":0,"viewOnce":false, "attachments": [{"contentType": "image/png", "filename": "image.png", "id": "4296180834490578536","size": 12005}]}},"account":"+49987654321","subscription":0}'  # noqa
 
     expected_source = "+490123456789"
     expected_timestamp = 1632576001632
     expected_text = "Uhrzeit"
     expected_group = "<groupid>"
+
+    signal_service = "127.0.0.1:8080"
+    phone_number = "+49123456789"
+
+    group_id = "group_id1"
+    group_secret = "group.group_secret1"
+    groups = {group_id: group_secret}
+
+    def setUp(self):
+        self.signal_api = SignalAPI(
+            TestMessage.signal_service, TestMessage.phone_number
+        )
 
     # Own Message
     async def test_parse_source_own_message(self):
@@ -58,6 +75,17 @@ class TestMessage(unittest.TestCase):
     async def test_read_reaction(self):
         message = await Message.parse(self.signal_api, TestMessage.raw_reaction_message)
         self.assertEqual(message.reaction, "👍")
+
+    @patch("aiohttp.ClientSession.get", new_callable=AsyncMock)
+    async def test_attachments(self, mock):
+        mock.return_value = b"test"
+        expected_base64_bytes = base64.b64encode(mock.return_value)
+        expected_base64_str = str(expected_base64_bytes, encoding="utf-8")
+
+        message = await Message.parse(
+            self.signal_api, TestMessage.raw_attachment_message
+        )
+        self.assertEqual(message.base64_attachments, [expected_base64_str])
 
     # User Chats
     async def test_parse_user_chat_message(self):

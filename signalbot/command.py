460b8acdbd0de57cd0f69b661e9b8dfe3185c060
Grant Bacon <btnarg+github@gmail.com>
2025-01-09 02:28:12 -0800

    Add decorator for matching regular expressions

--- signalbot/command.py
+++ signalbot/command.py
@@ -1,8 +1,27 @@
 import functools
+import re
 from abc import ABC, abstractmethod
 
 from .message import Message
 from .context import Context
+
+
+def regex_triggered(*by):
+    def decorator_regex_triggered(func):
+        @functools.wraps(func)
+        async def wrapper_regex_triggered(*args, **kwargs):
+            c = args[1]
+            text = c.message.text
+            if not isinstance(text, str):
+                return
+            matches = [bool(re.search(pattern, text)) for pattern in by]
+            if True not in matches:
+                return
+            return await func(*args, **kwargs)
+
+        return wrapper_regex_triggered
+
+    return decorator_regex_triggered
 
 
 def triggered(*by, case_sensitive=False):
--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import patch
 import logging
 from signalbot import Command, Context, triggered
+from signalbot.command import regex_triggered
 from signalbot.utils import ChatTestCase, SendMessagesMock, ReceiveMessagesMock
 
 
@@ -21,6 +22,15 @@ class TriggeredCaseSensitiveCommand(Command):
     @triggered("Trump", "Biden", case_sensitive=True)
     async def handle(self, c: Context):
         await c.send("I am triggered")
+
+
+class RegexTriggeredCommand(Command):
+    def describe(self) -> str:
+        return "😤 Triggered Command: Regular Expression Decorator Example"
+
+    @regex_triggered(r"\w+@\w+\.\w+", r"\d{3}-\d{3}-\d{4}")
+    async def handle(self, c: Context):
+        await c.send("I am triggered by regular expressions")
 
 
 class TriggeredTest(ChatTestCase):
@@ -70,6 +80,35 @@ class TriggeredCaseSensitiveTest(ChatTestCase):
     @patch("signalbot.SignalAPI.receive", new_callable=ReceiveMessagesMock)
     async def test_not_triggered(self, receive_mock, send_mock):
         receive_mock.define(["trump"])
+        await self.run_bot()
+        self.assertEqual(send_mock.call_count, 0)
+
+
+class RegexTriggeredTest(ChatTestCase):
+    def setUp(self):
+        super().setUp()
+        group = {"id": "asdf", "name": "Test"}
+        self.signal_bot._groups_by_internal_id = {"group_id1=": group}
+        self.signal_bot.register(RegexTriggeredCommand())
+
+    @patch("signalbot.SignalAPI.send", new_callable=SendMessagesMock)
+    @patch("signalbot.SignalAPI.receive", new_callable=ReceiveMessagesMock)
+    async def test_regex_triggered(self, receive_mock, send_mock):
+        receive_mock.define(["potus@whitehouse.tld"])
+        await self.run_bot()
+        self.assertEqual(send_mock.call_count, 1)
+
+    @patch("signalbot.SignalAPI.send", new_callable=SendMessagesMock)
+    @patch("signalbot.SignalAPI.receive", new_callable=ReceiveMessagesMock)
+    async def test_regex_triggered(self, receive_mock, send_mock):
+        receive_mock.define(["123-555-1234"])
+        await self.run_bot()
+        self.assertEqual(send_mock.call_count, 1)
+
+    @patch("signalbot.SignalAPI.send", new_callable=SendMessagesMock)
+    @patch("signalbot.SignalAPI.receive", new_callable=ReceiveMessagesMock)
+    async def test_not_regex_triggered(self, receive_mock, send_mock):
+        receive_mock.define(["11-222"])
         await self.run_bot()
         self.assertEqual(send_mock.call_count, 0)
 

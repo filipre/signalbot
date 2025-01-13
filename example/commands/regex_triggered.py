273f9e41255f911e5829b9bc397a45d9bcc88ced
Grant Bacon <btnarg+github@gmail.com>
2025-01-13 06:58:08 -0800

    Remove invalid comment

--- example/commands/regex_triggered.py
+++ example/commands/regex_triggered.py
@@ -5,7 +5,6 @@ class TriggeredCommand(Command):
     def describe(self) -> str:
         return "Regex 😤 Decorator example, matches sha1 OR md5 checksums"
 
-    # add case_sensitive=True for case sensitive triggers
     @regex_triggered(r"^[a-f0-9]{40}$", r"^[a-f0-9]{32}$")
     async def handle(self, c: Context):
         await c.send("I am triggered by regular expressions")

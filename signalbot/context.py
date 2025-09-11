78546a95c07a64097245954188b2dc944ead5c96
Era Dorta <era.dorta@proton.me>
2025-09-11 16:20:50 +0100

    fix: missing annotation in Context class

--- signalbot/context.py
+++ signalbot/context.py
@@ -10,7 +10,7 @@ if TYPE_CHECKING:
 
 
 class Context:
-    def __init__(self, bot: SignalBot, message: Message):  # noqa: ANN204
+    def __init__(self, bot: SignalBot, message: Message) -> None:
         self.bot = bot
         self.message = message
 

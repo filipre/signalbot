2fb61accb803f842a51f7e97b4795bb55a59b50f
Era Dorta <era.dorta@proton.me>
2026-02-07 18:18:11 +0100

    tests: add unit testing for group decorator

--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -30,6 +30,8 @@ class TestCommon(ChatTestCase):
         super().setup()
         group = {"id": ChatTestCase.group_id, "name": ChatTestCase.group_name}
         self.signal_bot._groups_by_internal_id = {ChatTestCase.group_internal_id: group}  # noqa: SLF001
+        self.signal_bot._groups_by_id = {ChatTestCase.group_id: group}  # noqa: SLF001
+        self.signal_bot._groups_by_name = {ChatTestCase.group_name: [group]}  # noqa: SLF001
 
     def mock_send_receive(
         self, mocker: MockerFixture
@@ -115,3 +117,48 @@ class TestRegexTriggered(TestCommon):
         receive_mock.define(["11-222"])
         await self.run_bot()
         assert send_mock.call_count == 0
+
+
+class TestTriggeredGroups(TestCommon):
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        super().setup()
+
+    async def _test_trigger(self, mocker: MockerFixture, call_count: int) -> None:
+        send_mock, receive_mock = self.mock_send_receive(mocker)
+        receive_mock.define(["Trump"])
+        await self.signal_bot._resolve_commands()  # noqa: SLF001
+        await self.run_bot()
+        assert send_mock.call_count == call_count
+
+    async def test_triggered_name(self, mocker: MockerFixture):
+        self.signal_bot.register(TriggeredCommand(), groups=[ChatTestCase.group_name])
+        await self._test_trigger(mocker, call_count=1)
+
+    async def test_triggered_id(self, mocker: MockerFixture):
+        self.signal_bot.register(TriggeredCommand(), groups=[ChatTestCase.group_id])
+        await self._test_trigger(mocker, call_count=1)
+
+    async def test_triggered_internal_id(self, mocker: MockerFixture):
+        self.signal_bot.register(
+            TriggeredCommand(), groups=[ChatTestCase.group_internal_id]
+        )
+        await self._test_trigger(mocker, call_count=1)
+
+    async def test_not_triggered_name(self, mocker: MockerFixture):
+        self.signal_bot.register(
+            TriggeredCommand(), groups=[ChatTestCase.group_name + "a"]
+        )
+        await self._test_trigger(mocker, call_count=0)
+
+    async def test_not_triggered_id(self, mocker: MockerFixture):
+        self.signal_bot.register(
+            TriggeredCommand(), groups=[ChatTestCase.group_id + "a"]
+        )
+        await self._test_trigger(mocker, call_count=0)
+
+    async def test_not_triggered_internal_id(self, mocker: MockerFixture):
+        self.signal_bot.register(
+            TriggeredCommand(), groups=[ChatTestCase.group_internal_id + "a"]
+        )
+        await self._test_trigger(mocker, call_count=0)

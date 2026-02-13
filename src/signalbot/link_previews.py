da0ace852cb8f2a38e8fae6e105f11f624025678
Era Dorta <era.dorta@proton.me>
2026-02-13 19:35:44 +0100

    Add docstrings to linpreview

--- src/signalbot/link_previews.py
+++ src/signalbot/link_previews.py
@@ -4,10 +4,19 @@ from pydantic import BaseModel
 
 
 class LinkPreview(BaseModel):
-    base64_thumbnail: str | None
-    title: str
-    description: str | None
-    url: str
+    """Dataclass to hold a link preview."""
 
-    #  This is the local filename for a received link preview
+    base64_thumbnail: str | None
+    """The base64 encoded thumbnail of the link preview, if available."""
+
+    title: str
+    """The title of the link preview."""
+
+    description: str | None
+    """The description of the link preview, if available."""
+
+    url: str
+    """The url of the link preview."""
+
     id: str | None = None
+    """This is the local filename for a received link preview."""

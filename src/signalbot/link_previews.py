b0ccc967458057678c88e77b4acad22cd79af0d5
Era Dorta <era.dorta@proton.me>
2026-02-14 01:06:05 +0100

    Better linkpreview docs

--- src/signalbot/link_previews.py
+++ src/signalbot/link_previews.py
@@ -4,19 +4,19 @@ from pydantic import BaseModel
 
 
 class LinkPreview(BaseModel):
-    """Dataclass to hold a link preview."""
+    """Dataclass to representing a link preview.
+
+    Attributes:
+        base64_thumbnail: The base64 encoded thumbnail of the link preview, if
+            available.
+        title: The title of the link preview.
+        description: The description of the link preview, if available.
+        url: The url of the link preview.
+        id: The local filename for a received link preview.
+    """
 
     base64_thumbnail: str | None
-    """The base64 encoded thumbnail of the link preview, if available."""
-
     title: str
-    """The title of the link preview."""
-
     description: str | None
-    """The description of the link preview, if available."""
-
     url: str
-    """The url of the link preview."""
-
     id: str | None = None
-    """This is the local filename for a received link preview."""

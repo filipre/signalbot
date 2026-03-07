8d06e615805fdd2ca4006245921f6ac25f1aa382
Era Dorta <era.dorta@proton.me>
2026-03-07 22:14:27 +0100

    Rename folder and add to init

--- src/signalbot/api/README.md
+++ src/signalbot/api/README.md
@@ -1,6 +1,6 @@
 # API classes
 
-The classes in this py-schema [folder](./py-schema/) were automatically generated from the [JSON Schema](https://json-schema.org/) files in the json-schema [folder](./json-schema).
+The classes in this py-schema [folder](./py_schema/) were automatically generated from the [JSON Schema](https://json-schema.org/) files in the json-schema [folder](./json_schema).
 To convert from JSON Schema to pydantic dataclasses the datamodel-code-generator [tool](https://datamodel-code-generator.koxudaxi.dev/) is used.
 
 Those files were copied over from the signal-cli [repository](https://github.com/AsamK/signal-cli).
@@ -10,10 +10,10 @@ To generate the files run this command at the root of the repository
 
 ```bash
 uv run datamodel-codegen \
---input ./src/signalbot/api/json-schema/ \
+--input ./src/signalbot/api/json_schema/ \
 --input-file-type jsonschema \
 --output-model-type pydantic_v2.BaseModel \
 --formatters ruff-check ruff-format \
 --snake-case-field \
---output ./src/signalbot/api/py-schema
+--output ./src/signalbot/api/py_schema
 ```
--- src/signalbot/api/__init__.py
+++ src/signalbot/api/__init__.py
@@ -1,3 +1,68 @@
+from signalbot.api.py_schema.admin_delete_schema import AdminDelete
+from signalbot.api.py_schema.attachment_data_schema import AttachmentData
+from signalbot.api.py_schema.attachment_schema import Attachment
+from signalbot.api.py_schema.call_message_schema import (
+    AnswerMessage,
+    BusyMessage,
+    CallMessage,
+    HangupMessage,
+    IceUpdateMessage,
+    OfferMessage,
+)
+from signalbot.api.py_schema.contact_address_schema import ContactAddress
+from signalbot.api.py_schema.contact_avatar_schema import ContactAvatar
+from signalbot.api.py_schema.contact_email_schema import ContactEmail
+from signalbot.api.py_schema.contact_name_schema import ContactName
+from signalbot.api.py_schema.contact_phone_schema import ContactPhone
+from signalbot.api.py_schema.contact_schema import Contact
+from signalbot.api.py_schema.data_message_schema import DataMessage
+from signalbot.api.py_schema.edit_message_schema import EditMessage
+from signalbot.api.py_schema.error_schema import Error
+from signalbot.api.py_schema.group_info_schema import GroupInfo
+from signalbot.api.py_schema.internal_schema import Internal
+from signalbot.api.py_schema.mention_schema import Mention
+from signalbot.api.py_schema.message_envelope_schema import MessageEnvelope
+from signalbot.api.py_schema.payment_schema import Payment
+from signalbot.api.py_schema.pin_message_schema import PinMessage
+from signalbot.api.py_schema.poll_create_schema import PollCreate
+from signalbot.api.py_schema.poll_terminate_schema import PollTerminate
+from signalbot.api.py_schema.poll_vote_schema import PollVote
+from signalbot.api.py_schema.preview_schema import Preview
+from signalbot.api.py_schema.profile_schema import Profile
+from signalbot.api.py_schema.quote_schema import Quote
+from signalbot.api.py_schema.quoted_attachment_schema import QuotedAttachment
+from signalbot.api.py_schema.reaction_schema import Reaction
+from signalbot.api.py_schema.receipt_message_schema import ReceiptMessage
+from signalbot.api.py_schema.recipient_address_schema import RecipientAddress
+from signalbot.api.py_schema.remote_delete_schema import RemoteDelete
+from signalbot.api.py_schema.send_message_result_schema import SendMessageResult
+from signalbot.api.py_schema.send_message_result_schema import (
+    Type as SendMessageResultType,
+)
+from signalbot.api.py_schema.shared_contact_schema import SharedContact
+from signalbot.api.py_schema.sticker_schema import Sticker
+from signalbot.api.py_schema.story_context_schema import StoryContext
+from signalbot.api.py_schema.story_message_schema import (
+    BackgroundGradient as StoryBackgroundGradient,
+)
+from signalbot.api.py_schema.story_message_schema import StoryMessage
+from signalbot.api.py_schema.story_message_schema import (
+    TextAttachment as StoryTextAttachment,
+)
+from signalbot.api.py_schema.sync_data_message_schema import SyncDataMessage
+from signalbot.api.py_schema.sync_message_schema import SyncMessage
+from signalbot.api.py_schema.sync_message_schema import Type as SyncMessageType
+from signalbot.api.py_schema.sync_read_message_schema import SyncReadMessage
+from signalbot.api.py_schema.sync_story_message_schema import (
+    BackgroundGradient as SyncStoryBackgroundGradient,
+)
+from signalbot.api.py_schema.sync_story_message_schema import SyncStoryMessage
+from signalbot.api.py_schema.sync_story_message_schema import (
+    TextAttachment as SyncStoryTextAttachment,
+)
+from signalbot.api.py_schema.text_style_schema import TextStyle
+from signalbot.api.py_schema.typing_message_schema import TypingMessage
+from signalbot.api.py_schema.unpin_message_schema import UnpinMessage
 from signalbot.api.signal_api import (
     ConnectionMode,
     ReceiveMessagesError,
@@ -5,4 +70,62 @@ from signalbot.api.signal_api import (
     SignalAPI,
 )
 
-__all__ = ["ConnectionMode", "ReceiveMessagesError", "SendMessageError", "SignalAPI"]
+__all__ = [
+    "AdminDelete",
+    "AnswerMessage",
+    "Attachment",
+    "AttachmentData",
+    "BusyMessage",
+    "CallMessage",
+    "ConnectionMode",
+    "Contact",
+    "ContactAddress",
+    "ContactAvatar",
+    "ContactEmail",
+    "ContactName",
+    "ContactPhone",
+    "DataMessage",
+    "EditMessage",
+    "Error",
+    "GroupInfo",
+    "HangupMessage",
+    "IceUpdateMessage",
+    "Internal",
+    "Mention",
+    "MessageEnvelope",
+    "OfferMessage",
+    "Payment",
+    "PinMessage",
+    "PollCreate",
+    "PollTerminate",
+    "PollVote",
+    "Preview",
+    "Profile",
+    "Quote",
+    "QuotedAttachment",
+    "Reaction",
+    "ReceiptMessage",
+    "ReceiveMessagesError",
+    "RecipientAddress",
+    "RemoteDelete",
+    "SendMessageError",
+    "SendMessageResult",
+    "SendMessageResultType",
+    "SharedContact",
+    "SignalAPI",
+    "Sticker",
+    "StoryBackgroundGradient",
+    "StoryContext",
+    "StoryMessage",
+    "StoryTextAttachment",
+    "SyncDataMessage",
+    "SyncMessage",
+    "SyncMessageType",
+    "SyncReadMessage",
+    "SyncStoryBackgroundGradient",
+    "SyncStoryMessage",
+    "SyncStoryTextAttachment",
+    "TextStyle",
+    "TypingMessage",
+    "UnpinMessage",
+]
